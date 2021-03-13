a23cb30f2795930b309a1488fbf67da119304054
Logan MacLaren <maclarel@github.com>
2021-03-12 21:39:30 -0500

    adding NSG associations, dns zone/records, and lb configuration

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_subnet" "subnet" {
 }
 
 # Create Network Security Group and rule for GHES client ports
-resource "azurerm_network_security_group" "nsg" {
+resource "azurerm_network_security_group" "ghes_30_NSG" {
   name                = "ghes_30_NSG"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -164,6 +164,33 @@ resource "azurerm_network_interface" "app_1_nic" {
   }
 }
 
+# Associate NICs to NSG
+
+resource "azurerm_network_interface_security_group_association" "app_0_nic_nsg_assoc" {
+  network_interface_id          = azurerm_network_interface.app_0_nic.id
+  network_security_group_id     = azurerm_network_security_group.ghes_30_NSG.id
+}
+
+resource "azurerm_network_interface_security_group_association" "app_1_nic_nsg_assoc" {
+  network_interface_id          = azurerm_network_interface.app_1_nic.id
+  network_security_group_id     = azurerm_network_security_group.ghes_30_NSG.id
+}
+
+resource "azurerm_network_interface_security_group_association" "data_0_nic_nsg_assoc" {
+  network_interface_id          = azurerm_network_interface.data_0_nic.id
+  network_security_group_id     = azurerm_network_security_group.ghes_30_NSG.id
+}
+
+resource "azurerm_network_interface_security_group_association" "data_1_nic_nsg_assoc" {
+  network_interface_id          = azurerm_network_interface.data_1_nic.id
+  network_security_group_id     = azurerm_network_security_group.ghes_30_NSG.id
+}
+
+resource "azurerm_network_interface_security_group_association" "data_2_nic_nsg_assoc" {
+  network_interface_id          = azurerm_network_interface.data_2_nic.id
+  network_security_group_id     = azurerm_network_security_group.ghes_30_NSG.id
+}
+
 # Create VMs
 
 resource "azurerm_virtual_machine" "ghes_30_data_0" {
@@ -218,12 +245,135 @@ resource "azurerm_public_ip" "ghes_30_lb_publicip" {
   sku                 = var.azure_sku
 }
 
-resource "azurerm_public_ip" "ghes_30_lb" {
+resource "azurerm_lb" "ghes_30_lb" {
   name                          = "ghes_30_lb"
   location                      = var.location
   resource_group_name           = azurerm_resource_group.rg.name
-  allocation_method             = "Static"
   sku                           = var.azure_sku
 
-  # We need to come back and define frontend/backend/rules
+  frontend_ip_configuration {
+    name                 = "ghes_30_lb_public_ip"
+    public_ip_address_id = azurerm_public_ip.ghes_30_lb_publicip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "ghes_30_lb_backend_address_pool" {
+  name                      = "ghes_30_lb_backend_address_pool"
+  loadbalancer_id           = azurerm_lb.ghes_30_lb.id
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "ghes_30_lb_pool_assoc_app_0" {
+  network_interface_id    = azurerm_network_interface.app_0_nic.id
+  ip_configuration_name   = "app_0_NICConfg"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.ghes_30_lb_backend_address_pool.id
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "ghes_30_lb_pool_assoc_app_1" {
+  network_interface_id    = azurerm_network_interface.app_1_nic.id
+  ip_configuration_name   = "app_1_NICConfg"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.ghes_30_lb_backend_address_pool.id
+}
+
+# Create load balancer health probe
+
+resource "azurerm_lb_probe" "ghes_30_lb_probe" {
+  resource_group_name = azurerm_resource_group.rg.name
+  loadbalancer_id     = azurerm_lb.ghes_30_lb.id
+  name                = "ghes_30_cluster_health_probe"
+  port                = 443
+}
+
+# Create Load Balancer Rules
+
+resource "azurerm_lb_rule" "ghes_30_lb_rule_TCP_22" {
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.ghes_30_lb.id
+  name                           = "LB_TCP_22"
+  protocol                       = "Tcp"
+  frontend_port                  = 22
+  backend_port                   = 22
+  frontend_ip_configuration_name = "ghes_30_lb_public_ip"
+}
+
+resource "azurerm_lb_rule" "ghes_30_lb_rule_TCP_80" {
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.ghes_30_lb.id
+  name                           = "LB_TCP_80"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "ghes_30_lb_public_ip"
+}
+
+resource "azurerm_lb_rule" "ghes_30_lb_rule_TCP_122" {
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.ghes_30_lb.id
+  name                           = "LB_TCP_122"
+  protocol                       = "Tcp"
+  frontend_port                  = 122
+  backend_port                   = 122
+  frontend_ip_configuration_name = "ghes_30_lb_public_ip"
+}
+
+resource "azurerm_lb_rule" "ghes_30_lb_rule_TCP_443" {
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.ghes_30_lb.id
+  name                           = "LB_TCP_443"
+  protocol                       = "Tcp"
+  frontend_port                  = 443
+  backend_port                   = 443
+  frontend_ip_configuration_name = "ghes_30_lb_public_ip"
+}
+
+resource "azurerm_lb_rule" "ghes_30_lb_rule_TCP_8080" {
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.ghes_30_lb.id
+  name                           = "LB_TCP_8080"
+  protocol                       = "Tcp"
+  frontend_port                  = 8080
+  backend_port                   = 8080
+  frontend_ip_configuration_name = "ghes_30_lb_public_ip"
+}
+
+resource "azurerm_lb_rule" "ghes_30_lb_rule_TCP_8443" {
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.ghes_30_lb.id
+  name                           = "LB_TCP_8443"
+  protocol                       = "Tcp"
+  frontend_port                  = 8443
+  backend_port                   = 8443
+  frontend_ip_configuration_name = "ghes_30_lb_public_ip"
+}
+
+resource "azurerm_lb_rule" "ghes_30_lb_rule_TCP_9418" {
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.ghes_30_lb.id
+  name                           = "LB_TCP_9418"
+  protocol                       = "Tcp"
+  frontend_port                  = 9418
+  backend_port                   = 9418
+  frontend_ip_configuration_name = "ghes_30_lb_public_ip"
+}
+
+# Create DNS Zone & CNAME & A Record
+
+resource "azurerm_dns_zone" "ghes_30_dns_zone" {
+  name                = "tsamcluster.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_dns_cname_record" "cname" {
+  name                = "cname"
+  zone_name           = azurerm_dns_zone.ghes_30_dns_zone.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  record              = "tsamcluster.net"
+}
+
+resource "azurerm_dns_a_record" "example" {
+  name                = "30"
+  zone_name           = azurerm_dns_zone.ghes_30_dns_zone.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  target_resource_id  = azurerm_public_ip.ghes_30_lb_publicip.id
 }
